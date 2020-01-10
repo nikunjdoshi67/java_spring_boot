3e8c50a9bc1992d017a9df950f1ee5549f3ec478
nikunjdoshi23 <ndoshi@redhat.com>
2020-01-10 16:13:23 +0000

    made changes

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -46,6 +46,13 @@ import org.springframework.samples.petclinic.model.Person;
 @Table(name = "owners")
 public class Owner extends Person {
 
+
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Column(name = "address")
 	@NotEmpty
 	private String address;
